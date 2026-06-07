9e585c4bed00731328532266a0885976147a1b1c
Sherif Mostafa <sherifmostafagaber@gmail.com>
2026-06-08 02:04:11 +0300

    Fix: Correct connect-mongo v6.0.0 import (named destructured export required)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express      = require('express');
 const session      = require('express-session');
-const MongoStore = require('connect-mongo');
+const { MongoStore } = require('connect-mongo');
 const cors         = require('cors');
 const path         = require('path');
 const fs           = require('fs');
